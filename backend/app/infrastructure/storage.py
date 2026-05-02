9036a334791ff19606effb3c741bd1b62b504145
Vitor Souza <tutornews2@gmail.com>
2026-05-01 23:56:50 -0300

    fix: error

--- backend/app/infrastructure/storage.py
+++ backend/app/infrastructure/storage.py
@@ -21,7 +21,11 @@ def _get_s3_client():
         aws_access_key_id=settings.s3_access_key,
         aws_secret_access_key=settings.s3_secret_key,
         region_name=settings.s3_region,
-        config=Config(signature_version="s3v4")
+        config=Config(
+            signature_version="s3v4",
+            s3={"addressing_style": "path"},
+            retries={"max_attempts": 3, "mode": "standard"},
+        ),
     )
     return _s3_client
 
